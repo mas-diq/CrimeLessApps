253ff83e5dfee40e8abf1a93acb6ea7ae830cda8
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-05-28 19:59:24 +0700

    Update layout

---
+++ app/src/main/java/com/dicoding/crimelessapps/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.dicoding.crimelessapps
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.crimelessapps.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        // Delay splash 1 sec
+        Handler(mainLooper).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
