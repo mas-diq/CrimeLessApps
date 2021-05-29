afe603f3285e1d4c5cb53a5e0c97d340a79f1960
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-05-29 19:18:22 +0700

    Adding layout login & register

---
+++ app/src/main/java/com/dicoding/crimelessapps/ui/Authentication/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.crimelessapps.ui.Authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.crimelessapps.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
