c79a9a8a3b630e620188b692eb4b401b230a999b
Muhammad Shiddiq Fathullah <msf31@student.gunadarma.ac.id>
2021-06-06 17:32:13 +0700

    Add DetailActivity.kt

--- app/src/main/java/com/dicoding/crimelessapps/DetailActivity.kt
+++ app/src/main/java/com/dicoding/crimelessapps/DetailActivity.kt
@@ -1,11 +1,16 @@
 package com.dicoding.crimelessapps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class DetailActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_USER = "extra_user"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
     }
 }
